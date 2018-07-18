50651a13fb3f0993d5e62894b321a8db1453b299
Yee Hong Chu <tobiascyh@users.noreply.github.com>
2018-07-17 23:25:50 -0400

    Add files via upload

---
+++ sample-c-code/network-simulation/n2.c
@@ -0,0 +1,138 @@
+/*
+  Author       : Yee Hong Chu
+  ID           : 0773285
+  Course       : CIS3210
+  Date created : 10/04/2013
+  Date modified: 11/09/2013
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_BUF 128
+
+/* Node2 is linked to Node1 */
+/* Node4 is linked to Node2 */
+int main(int argc, char *argv[])
+{
+    int fd, out_loop, in_loop, i;
+    char message[MAX_BUF] = "";
+    char buffer[MAX_BUF] = "";
+    char receiver[MAX_BUF] = "";
+    char ack[7] = "Nx_ack";
+    char stop[8] = "N2_stop";
+    char pipe1[12] = "./tmp/link1";
+    char pipe3[12] = "./tmp/link3";
+
+    /* Create named pipes with read and write permission */
+    mkfifo(pipe3, S_IRUSR| S_IWUSR);
+
+    /* Loop the program until user chooses to quit */
+    i = 0;
+    out_loop = 1;
+    while(out_loop)
+    {
+        /* Keep reading the pipe for incoming message */
+        in_loop = 1;
+        while(in_loop)
+        {
+            fd = open(pipe1, O_RDONLY);
+            read(fd, buffer, MAX_BUF);
+            close(fd);
+            if(strlen(buffer) == 3)
+            {
+                /* Check if this node is the destination node */
+                if(buffer[2] == '2')
+                {
+                    message[i] = buffer[0];
+                    if(buffer[0] != EOF)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        message[i] = '\0';
+                        i = 0;
+                        in_loop = 0;
+                        printf("N2, message received:\n%s\n", message);
+                        ack[1] = buffer[2];
+                    }
+                }
+                else
+                {
+                    /* Open the pipe to write into it and then closing it */
+                    fd = open(pipe3, O_WRONLY);
+                    write(fd, buffer, strlen(buffer));
+                    close(fd);
+
+                    /* Keep reading the pipe for incoming message */
+                    in_loop = 1;
+                    while(in_loop)
+                    {
+                        fd = open(pipe3, O_RDONLY);
+                        if(read(fd, receiver, MAX_BUF) >= 0)
+                        {
+                            in_loop = 0;
+                        }
+                        close(fd);
+                    }
+                    ack[1] = buffer[2];
+                }
+
+                /* Open the pipe to write into it and then closing it */
+                fd = open(pipe1, O_WRONLY);
+                write(fd, ack, strlen(ack));
+                close(fd);
+            }
+            else
+            {
+                if(strlen(buffer) == 1)
+                {
+                    in_loop = 0;
+
+                    /* Open the pipe to write into it and then closing it */
+                    fd = open(pipe3, O_WRONLY);
+                    write(fd, buffer, strlen(buffer));
+                    close(fd);
+
+                    /* Keep reading the pipe for incoming message */
+                    in_loop = 1;
+                    while(in_loop)
+                    {
+                        fd = open(pipe3, O_RDONLY);
+                        if(read(fd, receiver, MAX_BUF) >= 0)
+                        {
+                            in_loop = 0;
+                        }
+                        close(fd);
+                    }
+                    printf("N2, %s received\n", receiver);
+                }
+            }
+        }
+
+        /* Check user input for stop signal */
+        if(strcmp(buffer, "q") == 0 || strcmp(buffer, "Q") == 0)
+        {
+            out_loop = 0;
+
+            /* Open the pipe to write into it and then closing it */
+            fd = open(pipe1, O_WRONLY);
+            write(fd, stop, strlen(stop));
+            close(fd);
+        }
+
+        /* Reset the buffer and message */
+        memset(&buffer, 0, MAX_BUF);
+        memset(&message, 0, MAX_BUF);
+    }
+
+    /* Remove the created named pipes */
+    unlink(pipe3);
+
+    return 0;
+}
---
+++ sample-c-code/network-simulation/n6.c
@@ -0,0 +1,88 @@
+/*
+  Author       : Yee Hong Chu
+  ID           : 0773285
+  Course       : CIS3210
+  Date created : 10/04/2013
+  Date modified: 11/09/2013
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_BUF 128
+
+/* Node6 is linked to Node3 */
+int main(int argc, char *argv[])
+{
+    int fd, out_loop, in_loop, i;
+    char message[MAX_BUF] = "";
+    char buffer[MAX_BUF] = "";
+    char ack[7] = "ack";
+    char stop[8] = "N6_stop";
+    char pipe5[12] = "./tmp/link5";
+
+    /* Loop the program until user chooses to quit */
+    i = 0;
+    out_loop = 1;
+    while(out_loop)
+    {
+        /* Keep reading the pipe for incoming message */
+        in_loop = 1;
+        while(in_loop)
+        {
+            fd = open(pipe5, O_RDONLY);
+            read(fd, buffer, MAX_BUF);
+            close(fd);
+            if(strlen(buffer) == 3)
+            {
+                message[i] = buffer[0];
+                /* Check for end-of-message transmission */
+                if(buffer[0] != EOF)
+                {
+                    i++;
+                }
+                else
+                {
+                    message[i] = '\0';
+                    i = 0;
+                    in_loop = 0;
+                    printf("N6, message received:\n%s\n", message);
+                }
+
+                /* Acknowledge receiving packet */
+                fd = open(pipe5, O_WRONLY);
+                write(fd, ack, strlen(ack));
+                close(fd);
+            }
+            else
+            {
+                /* Keep reading if buffer is empty */
+                if(strlen(buffer) == 1)
+                {
+                    in_loop = 0;
+                }
+            }
+        }
+
+        /* Check packet for stop signal */
+        if(strcmp(buffer, "q") == 0 || strcmp(buffer, "Q") == 0)
+        {
+            out_loop = 0;
+
+            /* Acknowledge stop signal to sender node */
+            fd = open(pipe5, O_WRONLY);
+            write(fd, stop, strlen(stop));
+            close(fd);
+        }
+
+        /* Reset the buffer and message */
+        memset(&buffer, 0, MAX_BUF);
+        memset(&message, 0, MAX_BUF);
+    }
+
+    return 0;
+}
