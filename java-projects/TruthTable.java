9fe8b087155726224b8e4d7fe948640b756d784d
Yee Hong Chu <tobiascyh@users.noreply.github.com>
2018-07-24 22:34:16 -0400

    Create TruthTable.java

---
+++ java-projects/TruthTable.java
@@ -0,0 +1,34 @@
+/**
+ * Truth table for the logical operators using true and false.
+ * @author Tobias Chu
+ *
+ */
+public class TruthTable
+{
+    public static void main(String[] args)
+    {
+        boolean p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+        p = true; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = true; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+    }
+}
